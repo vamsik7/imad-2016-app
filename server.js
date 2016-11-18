ce1accd89e5ac0897d8157cda790f0a138224b34
vamsik7 <cs11b007@iith.ac.in>
2016-11-18 18:42:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').Pool;
+var Pool = require('pg').Pool;
 
 var config = {
     user: "vamsik7",
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 });
 
 // test db
-//var pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/testdb',function(req,res){
     
     pool.query('select * from article', function(err, result){
