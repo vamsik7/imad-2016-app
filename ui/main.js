d2e6f76d56624fb8bac41f39edccc4b305f32a4f
vamsik7 <cs11b007@iith.ac.in>
2016-11-18 12:26:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick = function() {
     //make the request
     var nameInput = document.getElementById('name');
     var name= nameInput.value;
-    request.open('GET','http://vamsik7.imad.hasura-app.io/submit-name/name='+ name, true );
+    request.open('GET','http://vamsik7.imad.hasura-app.io/submit-name/?name='+ name, true );
     request.send(null);
     
    
