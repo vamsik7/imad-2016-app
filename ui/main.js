d8afef3f79aaba70574ebdd0c2a9747dd33b41dd
vamsik7 <cs11b007@iith.ac.in>
2016-11-18 12:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,62 @@
 console.log('Loaded!');
-var content = document.getElementById('content');
+
+var submit = document.getElementById('submit-btn');
+submit.onclick = function (){
+    // request the sever and get it back
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+        if(request.readystatechange === XMLHttpRequest.DONE){
+            if(request.readystatus === 200){
+                 // display in the html
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++){
+                     list += '<li>' + name[i] + '</li>';
+                }
+                var ulname = document.getElementById('submit-list');
+                ulname.innerHTML = list;
+            }
+            
+        }else{
+            
+        }
+    };
+    var nameInput = document.getElementById('name');
+    var name= nameInput.value;
+    request.open('GET','http://vamsik7.imad.hasura-app.io/submit-name/name='+ name, true );
+    request.send(null);
+    
+   
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var content = document.getElementById('content');
 var madi = document.getElementById('madi');
 
 content.innerHTML = "This is webappzz";
@@ -13,4 +70,4 @@ function move(){
 madi.onclick = function (){
   //  madi.style.marginLeft = '100px';
    var interval=  setInterval(move,10);
-}
+}*/
