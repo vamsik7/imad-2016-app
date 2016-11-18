4e3528f30310686f3a5e700774f6219e351abaac
vamsik7 <cs11b007@iith.ac.in>
2016-11-18 10:46:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/', function (req,res){
+    counter +=1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
