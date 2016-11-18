615caddd809badb8def91cc6f95ad00f6dac3c8a
vamsik7 <cs11b007@iith.ac.in>
2016-11-18 09:34:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,83 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    
+    'article-one':{
+        'title': "Article One, vamsi",
+        'date': '9 sep 12',
+        'content':`<p>
+                What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??
+            </p>
+             <p>
+                What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??
+            </p>`
+    },
+    'article-two':{
+         'title': "Article Two, vamsi",
+        'date': '19 sep 12',
+        'content':`<p>
+                2What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??
+            </p>
+             <p>
+                What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??
+            </p>`
+    },
+    'article-three':{
+         'title': "Article Three, vamsi",
+        'date': '29 sep 12',
+        'content':`<p>
+                3What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??
+            </p>
+             <p>
+                What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??What the hell are you doing in this first one ??
+            </p>`
+    }
+};
+
+
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate =`
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        </head>
+         <body>
+             <div>
+                 <a href="/">Home</a>
+             </div>
+            <hr> 
+            <h3>
+                  ${title}
+            </h3>
+            <h5>
+                ${date}
+            </h5>
+            <div>
+                ${content}
+            </div>
+         </body>
+    </html>`;
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
+app.get('/:articleName', function(req,res){
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-three', function(req,res){
-    res.send('Article three arrived');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
