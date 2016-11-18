500f46508afe14657806445bc12b583bd3719232
vamsik7 <cs11b007@iith.ac.in>
2016-11-18 10:19:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+var madi = document.getElementById('madi');
+
+function move(){
+    
+    madi.style.marginleft = '100px';
+}
+
+madi.onClick = function (){
+    setInterval(move,500);
+}
