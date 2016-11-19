97124858c8960196ab8f39027235fafa01a685ee
vamsik7 <cs11b007@iith.ac.in>
2016-11-19 12:58:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ window.onload = function() {
      var loginHtml=`
         <div class="login_js">
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <h4>Login</h4>
         <input type="text" id="username" placeholder="username" name="firstname" class="col-md-6"><br/>
