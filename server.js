556d54edc4ad6d6f4add8fcc68b250edb31eeb15
vamsik7 <cs11b007@iith.ac.in>
2016-11-19 11:00:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/get-articles', function (req, res) {
 app.get('/get-comments/:articleName', function (req, res) {
    // make a select request
    // return a response with the results
-   pool.query('SELECT comment.*, "users".username FROM articles, comments, "users" WHERE articles.title = $1 AND articles.id = comments.article_id AND comments.user_id = "users".id ORDER BY comments.timestamp DESC', [req.params.articleName], function (err, result) {
+   pool.query('SELECT comments.*, "users".username FROM articles, comments, "users" WHERE articles.title = $1 AND articles.id = comments.article_id AND comments.user_id = "users".id ORDER BY comments.timestamp DESC', [req.params.articleName], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
