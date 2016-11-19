77a9cfc110dfb8eec55338eff5c1e52c69577731
vamsik7 <cs11b007@iith.ac.in>
2016-11-19 15:13:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,14 @@
 
 window.onload = function() {
  
+ function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+ 
  function loadLoginForm () {
      var loginHtml=`
         <div class="login_js">
