019c846fcf8140c8c01798eb7f1ee662f070bc16
vamsik7 <cs11b007@iith.ac.in>
2016-11-19 12:26:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function createTemplate (data) {
         <script type="text/javascript" src="/ui/articles.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="/ui/main.js"></script>
+        
 	    
         
     </body>
