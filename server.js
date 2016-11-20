1e109f13da391d1ada64f72a75618a4b62211161
vamsik7 <cs11b007@iith.ac.in>
2016-11-20 10:37:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,84 +150,6 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
-function loadCatArticles (articleData) {
-        // Check if the user is already logged in
-  
-   var htmlTemplate =`
-        <html>
-    <head>
-         <title>
-            ${articleData[0].category};
-         </title>
-        <link href="/ui/style.css" rel="stylesheet" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
-    </head>
-    
-    <body>
-        <div class="navbar" id="navbar">
-            <div>
-                
-            </div>
-			      
-		</div>  
-        <div  class="container">
-			
-			
-			<div class="post_area">
-			   <div id="articles">
-			   <script>
-			       articles = document.getElementById('articles');
-			        var content="<ul>";
-			        for (var i=0; i< articleData.length; i++) {
-                    
-                    content+=`
-                    <div id="short_article">
-                        <h4><a href="/articles/${articleData[i].title}">${articleData[i].heading}</a></h4>
-							<h5>(${articleData[i].date.split('T')[0]})</h5>
-							<div>${(articleData[i].content).substr(0,200)}... <a id='rmlink' style='float:right;' href="/articles/${articleData[i].title}">Read More</a></div><br/><hr/>
-                    </div>
-                    `;
-                    
-                 }
-                content += "</ul>";
-                articles.innerHTML = content;
-			   </script>
-			   
-			   
-			
-			<div class="side_bar">
-			     <div id="login_area">
-                  
-                       <a href="/">Recent Posts</a>
-     
-               </div>
-			     
-			</div>
-			
-			
-	    </div>
-	    <div class="footer">
-			    <h5 style="paddding:20px; text-align:center">All rights reserved &copy; - Vamsi, email: cs11b007@iith.ac.in</h5>
-		</div>
-	    
-	    <script type="text/javascript" src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-        <script type="text/javascript">tinymce.init({ selector:'textarea' });</script>
-        <script type="text/javascript" src="/ui/articles.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        
-	    
-        
-    </body>
-   
-     
-    </html>
-   `;
-   return htmlTemplate;
-            
-    
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -402,21 +324,6 @@ app.get('/articles/:articleName', function (req, res) {
 });
 
 
-app.get('/categories/:catName', function (req, res) {
-  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  pool.query("SELECT * FROM articles WHERE category = $1", [req.params.catName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Category is empty or not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(loadCatArticles(articleData));
-        }
-    }
-  });
-});
 
 
 app.get('/ui/:fileName', function (req, res) {
